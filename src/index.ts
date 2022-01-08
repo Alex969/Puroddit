2a68a91ddf9274409a1d77dbf152e7064db29439
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2022-01-08 23:52:38 +0100

    import mikro-orm and create new instance of it
    a new MikroOrm instance is created in const orm, we await it since MikroORM.init returns a promise and wrap it in a main async function to be able to use await on the promise

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello there ")
+import { MikroORM } from "@mikro-orm/core";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    
+  }); //returns a promise 
+}
+
+main();
