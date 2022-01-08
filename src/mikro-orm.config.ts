0a5683e3cd4019599d6e19770a1ad12f187bea30
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2022-01-09 00:38:06 +0100

    declared default mikro-orm object to be of type MikroOrm.init()

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,9 +1,14 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
 
 export default {
   entities: [Post],
   dbName: "Puroddit",
   type: "postgresql",
-  debug: !__prod__
-}
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init()>[0]; 
+/*Parameters returns an array 
+so we just select the first parameter*/
+
+
