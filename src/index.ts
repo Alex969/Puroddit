869d2180ebf6f8c855d7772f0b2ad9a4cd7f1d0c
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2022-01-09 00:05:46 +0100

    add dbName, type and debug to orm const
    dbName is the name of the database we're connecting to, type is the database type we're using (postgres in that case), debug lets us know of what the SQL code looks like under the hood

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
 
 const main = async () => {
   const orm = await MikroORM.init({
-    
+    dbName: "Puroddit",
+    type: "postgresql",
+    debug: !__prod__
   }); //returns a promise 
 }
 
