2b9108244cafb52c98e65bb565f468613b22a432
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2022-01-09 17:56:14 +0100

    finish mikro-orm setup

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ import microConfig from "./mikro-orm.config";
 const main = async () => {
   const orm = await MikroORM.init(microConfig); //init returns a promise 
   await orm.getMigrator().up(); //runs migrations
-  const post = orm.em.create(Post, {title: 'my first post 1'});
-  await orm.em.persistAndFlush(post);
-  console.log("----------sql 2----------");
-  await orm.em.nativeInsert(Post, {title: 'my first post 2'});
+  // const post = orm.em.create(Post, {title: 'my first post 1'});
+  // await orm.em.persistAndFlush(post);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
+
 }
 
 main().catch((err) => {
