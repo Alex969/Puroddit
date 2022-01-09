8917e954097807de3666fb9e1270269e286d3a70
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2022-01-09 18:03:15 +0100

    use express as server listening on port 3000

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,20 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express';
+
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig); //init returns a promise 
   await orm.getMigrator().up(); //runs migrations
-  // const post = orm.em.create(Post, {title: 'my first post 1'});
-  // await orm.em.persistAndFlush(post);
 
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
-
+  const app = express();
+  app.get('/', (_,res) => {
+    res.send("hello");
+  });
+  app.listen(3000, () => {
+    console.log("server started on localhost:3000");
+  })
 }
 
 main().catch((err) => {
